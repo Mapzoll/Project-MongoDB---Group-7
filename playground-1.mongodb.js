240bd836c14b653eff2509cfc23f2030ccc5405d
M Alfian Pratama <ian25pratama@gmail.com>
2026-05-15 10:42:34 +0700

    ganti query

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -3,15 +3,12 @@
 
 // The current database to use.
 use('MembershipDB');
-
-db.getCollection('Transaksi').aggregate([
+db.getCollection('Member Gym').aggregate([
   {
-    $lookup: {
-      from: "Member Gym",       // Koleksi asal
-      localField: "member_id",  // Field di koleksi Transaksi
-      foreignField: "_id",      // Field di koleksi Member Gym
-      as: "detail_member"       // Nama output field baru
+    $group: {
+      _id: null,
+      rataRataUmur: { $avg: "$umur" },
+      totalMember: { $sum: 1 }
     }
-  },
-  { $unwind: "$detail_member" } // Memecah array hasil lookup agar rapi
+  }
 ]);
